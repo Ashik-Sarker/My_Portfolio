ebffb50108cb58192a7ce9715a6e300ab9ec9517
Md.Abu Tahir Sarker <msarker141151@bscse.uiu.ac.bd>
2022-06-09 15:10:53 +0600

    first commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Banner from './Banner';
+import GetInTouch from './GetInTouch';
+import NavBar from './NavBar';
+import Projects from './Projects';
+import Skills from './Skills';
+
+const Home = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            {/* <Banner></Banner> */}
+            {/* <Skills></Skills> */}
+            {/* <Projects></Projects> */}
+            {/* <GetInTouch></GetInTouch> */}
+        </div>
+    );
+};
+
+export default Home;
